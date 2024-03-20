784430bf88ac6bf63d744951af810865b93f9e6c
Manuele Cavalli-Sforza <manuele.cavallisforza@gmail.com>
2024-03-19 23:59:49 -0400

    Exercise 2 - Guess List

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 
 import GuessInput from "../GuessInput";
+import GuessList from "../GuessList";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,9 +12,21 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guessList, setGuessList] = React.useState([]);
+
+  function addGuess(guess) {
+    const newGuess = {
+      guess,
+      id: crypto.randomUUID(),
+    };
+    const newGuessList = [...guessList, newGuess];
+    setGuessList(newGuessList);
+  }
+
   return (
     <>
-      <GuessInput />
+      <GuessList guessList={guessList} />
+      <GuessInput addGuess={addGuess} />
     </>
   );
 }
